89d18ad01b8d0d98cab65159ac71a314e7763aff
ashley <ashleyyytjh@gmail.com>
2023-08-30 16:28:07 +0800

    chore: adding layout and nav bar

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,41 @@
+import { AppBar, Toolbar, IconButton, Box, Typography, Button,  } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem/MenuItem';
+
+const Header = () => {
+    return (
+        <Box>
+        <AppBar position="static">
+       
+          <Toolbar>
+            {/* <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+            </IconButton> */}
+            <Box sx={{width: '100%', border: 1, display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Box sx={{display: 'flex'}}>
+                    <Typography variant="h6" component="div">
+                        Title
+                    </Typography>
+                </Box>
+                <Box sx={{display: 'flex'}}>
+                    <Button color = "inherit" component="div"> HOME </Button>
+                    <Button color = "inherit" component="div"> MOVIES </Button>
+                    <Button color="inherit">ACC</Button>
+                </Box>
+            </Box>
+            <Box>
+                
+            </Box>
+            
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+    )
+}
+
+export default Header;
---
+++ src/app/pages/Home.tsx
@@ -0,0 +1,15 @@
+import {Typography} from '@mui/material';
+
+
+const Home = () => {
+    return (
+        <div>
+            <Typography>
+                This is the home page
+            </Typography>
+        </div>
+
+    )
+}
+
+export default Home;
