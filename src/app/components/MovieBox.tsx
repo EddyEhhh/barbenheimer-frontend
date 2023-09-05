495da566023ef6487761b64e072b79e0cdffead9
ashley <ashleyyytjh@gmail.com>
2023-09-05 17:15:01 +0800

    feat:adding now showing section of home layout

--- src/app/components/MovieBox.tsx
+++ src/app/components/MovieBox.tsx
@@ -13,7 +13,7 @@ type MovieDetails = {
 const MovieBox = ({title, duration, picture, rating, ageRating} : MovieDetails) => {
     return (
         <Grid>
-             <Box sx ={{display:'flex', flexDirection:'column',border: 1, width:'300', height:'450'}}>
+            <Box sx ={{display:'flex', flexDirection:'column',border: 1, width:'300', height:'450'}}>
                 <Button sx= {{display:'flex', backgroundColor: 'secondary.white', border:1, height:'70%', width:'100%'}}>
                     <Box> insert img </Box>
                 </Button>
@@ -22,9 +22,11 @@ const MovieBox = ({title, duration, picture, rating, ageRating} : MovieDetails) 
                         <Typography variant="h6"> {title} insert title </Typography>
                         <Typography variant="subtitle1"> {ageRating} Age Rating PG13 </Typography>
                         <Typography variant="subtitle1"> {duration} Time 99 Minutes </Typography>
+                        <Button sx = {{mt:'4'}} variant="contained" color="success"  size="small"> Book now </Button>
                     </Stack>
                 </Box>
             </Box>
+
         </Grid>
    
     )
