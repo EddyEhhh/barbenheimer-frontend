c3c540e75192e98661f3286274694ba86919fbd8
ashley <ashleyyytjh@gmail.com>
2023-09-14 20:01:11 +0800

    feat: adding backend data to home page

--- src/app/components/NowShowing.tsx
+++ src/app/components/NowShowing.tsx
@@ -1,88 +1,98 @@
 "use client"
-import {Box, Typography, Grid, Pagination, Tabs, Tab, Paper} from "@mui/material/";
+import {Box, Typography, Grid, Pagination, Tabs, Tab} from "@mui/material/";
 import MovieBox from "./MovieBox";
 import * as React from 'react';
-import { useState } from "react";
+import { useState, useEffect, createContext, useContext } from "react";
+import AxiosInstance from "../api/AxiosInstance";
+import { getAllMovies } from "../services/homeServices";
+import { MovieDetailsInterface } from "../interface/interface";
+
+
 const NowShowingMovies = () => {
-    const [pageIndex, setPageIndex] = React.useState(1);
+    const [movieData, setMovieData] = useState<MovieDetailsInterface[][]>([[]]);
+    const [pageIndex, setPageIndex] = useState<number>(1);
+    const [value, setValue] = useState(0);
+    const wantedItemsPerPage: number = 10;
+    //xl-6(12) lg-5(10)
+    //how to make this dynamic?
+
+    useEffect (() => {
+        const fetchData = async () => {
+        try {
+            const data = await getAllMovies();
+            
+            let itemsPerPage: number = 0;
+            let index: number = 0; 
+
+            const pagination : MovieDetailsInterface[][] = [[]];
+            data.map((data : MovieDetailsInterface) => {
+                console.log(new Date(data.showingDate) <= new Date());
+                if (new Date(data.showingDate) <= new Date()){
+                    if (itemsPerPage == wantedItemsPerPage) {
+                        itemsPerPage = 0;
+                        index++;
+                        pagination[index] = [];
+                    }
+                    pagination[index][itemsPerPage] = data;
+                    itemsPerPage++;
+                }
+                
+            });
+            console.log(pagination);
+            setMovieData(pagination); 
+            
+// console.log(movieData[0][0].movieImages[0].imageUrl)
+            } catch(error) {
+                //inset error
+            }
+        }
+        fetchData();
+    }, [])
 
     const pageChangeHandler = async (event: React.ChangeEvent<unknown>, value: number) => {
         setPageIndex(value);
     }
-
-    const [nowShowing, setNowShowing] = useState<boolean>(true);
-    const [advancedSales, setAdvancedSales] = useState<boolean>(false);
-    const [comingSoon, setComingSoon] = useState<boolean>(false);
-    const [value, setValue] = useState(0);
 
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
     };
 
 
-    // const nowShowingHandler = () => {
-    //     if (nowShowing === false) {
-    //         setNowShowing(true);
-    //         setAdvancedSales(false);
-    //         setComingSoon(false);
-    //     }
-    // }
-
-    // const advancedSalesHandler = () => {
-    //     if (!advancedSales) {
-    //         setAdvancedSales(true);
-    //         setNowShowing(false);
-    //         setComingSoon(false);
-    //     }
-    // }
-
-    // const comingSoonHandler =() =>{
-    //     if (!comingSoon) {
-    //         setComingSoon(true);
-    //         setAdvancedSales(false);
-    //         setNowShowing(false);
-    //     }
-    // }
-
     return (
       <Box>
-        <Grid container justifyContent='center' alignItems='center' sx = {{pl:10, pr:10, mt:3, height:'fill'}}> 
-            {/*         
-            <Grid sx={{display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
-                <Typography onClick ={nowShowingHandler} color='white' fontWeight={nowShowing === true ? 'bold' : ''} variant = 'h6'> Now Showing </Typography>
-                <Typography onClick ={advancedSalesHandler} pl={8} color='white' fontWeight={advancedSales === true ? 'bold' : ''} variant = 'h6'> Advance Sales </Typography>
-                <Typography pl={8} onClick= {comingSoonHandler} color='white' fontWeight={comingSoon === true ? 'bold' : ''} variant = 'h6'> Coming Soon </Typography>
-            </Grid> */}
-       
+        <Grid container justifyContent='center' alignItems='center' sx = {{pl:10, pr:10, mt:3, height:'fill'}}>      
             <Tabs textColor="inherit" value={value} onChange={handleChange} centered>
                 <Tab sx={{fontSize:"medium"}} label="Now Showing"/>
                 <Tab sx={{fontSize:"medium"}} label="Advanced Sales" />
                 <Tab sx={{fontSize:"medium"}} label="Coming Soon" />
             </Tabs>
         
-
-         
-            <Box sx={{height:30, width:'1', mb:1, mt:1, display:"flex", alignItems:'center'}}>
+        <Box sx={{height:30, width:'1', mb:1, mt:1, display:"flex", alignItems:'center'}}>
                 <Typography color='white' fontWeight='bold' variant = 'h5'> Showtimes </Typography>
             </Box> 
           
             <Box sx = {{width:'100%'}}>
-                <Grid container columns={{lg:4, md:3, sm:2, xs:1}} columnSpacing={5} rowSpacing={2}>
-                    <MovieBox></MovieBox>
-                    <MovieBox></MovieBox>
-                    <MovieBox></MovieBox>
-                    <MovieBox></MovieBox>
-                    <MovieBox></MovieBox>
-                    <MovieBox></MovieBox>
-                    <MovieBox></MovieBox>
-                    <MovieBox></MovieBox>         
-                   
+                <Grid container columns={{lg:5, md:3, sm:2, xs:1}} columnSpacing={2} rowSpacing={2}>
+                    {
+                        movieData[pageIndex-1].map((movieData) => (
+                            <MovieBox 
+                                key={movieData.id} 
+                                id={movieData.id}
+                                title={movieData.title} 
+                                ageRestriction={movieData.ageRestriction} 
+                                runtimeInMinute={movieData.runtimeInMinute} 
+                                movieImages={movieData.movieImages[0].imageUrl}
+                            ></MovieBox>
+                        ))
+                    }
+                     
+                
                 </Grid>
             </Box>
 
             <Grid item sx ={{mt:10}}>
                 <Pagination 
-                    count={10} 
+                    count={movieData.length} 
                     showFirstButton showLastButton 
                     size="large" 
                     color="primary"
