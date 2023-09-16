5fbb0c9e807f1eb0be62da83868ca1dac226a366
ashley <ashleyyytjh@gmail.com>
2023-09-16 22:28:48 +0800

    feat: updating interfaces for movie date/timings

--- src/app/interface/interface.tsx
+++ src/app/interface/interface.tsx
@@ -10,5 +10,45 @@ export interface MovieDetailsInterface {
     lastShowingDate?: Date,
     showingDate: Date,
     ticketSaleDate?: Date
-    
+}
+
+
+export interface MovieSpecificDetailsInterface {
+    id:number,
+    title: string,
+    runtimeInMinute: number,
+    movieImages: {imageUrl: string}[],
+    rating?: number,
+    ageRestriction: number,
+    releaseDate: Date,
+    showingDate: Date,
+    ticketSaleDate?: Date,
+    basePrice?: number,
+    cast?: string,
+    description?: string,
+    language: string,
+    genre?: string,
+    director: string,
+}
+
+
+export interface MovieScheduleDatesInterface {
+    id: number,
+    showDate: string,
+    movieScheduleTimes: MovieScheduleTimeInterface[]
+    onClickHandler : () => void
+}
+
+
+export interface MovieScheduleTimeInterface {
+    id: number,
+    showTime :string,
+    hall: HallInterface[]
+
+}
+
+export interface HallInterface {
+    id: number,
+    seatCapacity: number,
+    seats: number[]
 }
